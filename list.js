7877a01f406186ee6737435175fad951a84a2829
ileuskov <ileuskov@gmail.com>
2020-04-01 16:53:19 +0200

    The functionality part is now completed

--- list.js
+++ list.js
@@ -1,5 +1,25 @@
-// Strikethrough effect for the Todos that are clicked
+// Strikethrough effect for the Todos that are clicked. "On" instead of "click" (because the future elements have to work too)
+$("ul").on("click", "li", function () {
+    $(this).toggleClass("completed");
+});
 
-$("li").click(function () {
-   $(this).toggleClass("completed");
+// Click on X to delete the Todo. "On" instead of "click" here too
+$("ul").on("click", "span", function (event) {
+    //Fading out the parent class of the span (in this case: "li" element)
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+    // Stopping the bubbling up to parent elements (so the function works only for span)
+    event.stopPropagation();
+});
+
+// Adding a new Todo by pressing the enter
+$("input[type = 'text']").keypress(function (event) {
+    if (event.which === 13) {
+        // taking the text input for a new Todo from the user. Clearing the input afterwards
+        var todoText = $(this).val();
+        $(this).val("");
+        // adding this text as a new li to the ul
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
 });
